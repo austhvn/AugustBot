3db4015b2aa60bd65d98506f61cd584f3514e0c3
Lidocaine <c14h22n2o@gmail.com>
2016-07-11 16:30:52 -0400

    Began rough structure for modularized commands and bot settings as determined in a yet-to-be-developed config.json file

--- augustbot.js
+++ augustbot.js
@@ -15,13 +15,69 @@ try {
 	process.exit();
 }
 
+// Config settings
+try {
+	var Config = require( './config.json' );
+} catch( e ) {
+	var Config = {};
+	Config.debug = true;
+	Config.commandPrefix = '+';
+}
+
 
 var AugustBot = new Discord.Client();
+var commands  = {
+	'ping' : {
+	  description: "",
+	  process: function( bot, msg, suff ) {
+	  	bot.sendMessage( msg.channel, "Pong, bitch!" );
+	  }
+	}
+};
 
-AugustBot.on( 'message', function( message ){
-	if( message.content === 'ping' ) {
-		AugustBot.reply( message, 'pong' );
+//AugustBot.on( 'message', function( message ){
+//	if( message.content === 'ping' ) {
+//		AugustBot.reply( message, 'pong' );
+//	}
+//});
+
+AugustBot.on( 'disconnected', function() {
+	console.log( 'Disconnected!' );
+	process.exit( 1 );
+});
+
+AugustBot.on( 'message', function( message ) {
+	if( message.author.id != AugustBot.user.id
+	    && ( message.content[ Config.commandPrefix.length - 1 ] === Config.commandPrefix
+	         || message.content.indexOf( AugustBot.user.mention() ) == 0 ) ){
+		
+		console.log( message.auther + ' attempting command: ' + message.content );
+	
+		var commandText = message.content.split( ' ' )[0].substring( 1 );
+		var commandSuffix = message.content.substring( commandText.length + 1 + Config.commandPrefix.length );
+		var cmd = commands[ commandText ];
+
+		if( cmd ) {
+			try {
+				AugustBot.reply( message, '`' + commandText + '` command found!' );
+			} catch (e) {
+				if( Config.debug ) {
+					AugustBot.reply( message, '`' + commandText + '` command failed to execute!\n' + e.stack );
+				}
+			}
+		} else {
+			if( Config.debug ) {
+				AugustBot.reply( message, '`' + commandText + '` command not found!\n' );
+			}
+		}
+
+	} else {
+		if( message.author == AugustBot.user ) return;
+		if( message.author != AugustBot.user && message.isMentioned( AugustBot.user ) ){
+			AugustBot.reply( message, "Shut the fuck up." );
+		}
 	}
 });
+
 
 AugustBot.loginWithToken( AuthInfo.token );
