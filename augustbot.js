7cf90aaee6aeaffcd9216a9c16fb252f937ad7ba
Lidocaine <c14h22n2o@gmail.com>
2016-07-12 20:53:52 -0400

    Ported Gif command I originally wrote a brief PHP script for use with Nightbot to pull a random gif from Giphy based on tags. I've now ported that command to AugustBot.

--- augustbot.js
+++ augustbot.js
@@ -1,10 +1,12 @@
 // Init Discord.js
 try {
 	var Discord = require( 'discord.js' );
+	var Request = require( 'request' );
 } catch( e ) {
 	console.log( e.stack );
 	console.log( process.version );
 	console.log( "Please run NPM install." );
+	process.exit();
 }
 
 // Auth data
@@ -22,16 +24,59 @@ try {
 	var Config = {};
 	Config.debug = true;
 	Config.commandPrefix = '+';
+	console.log( 'No configuration file found; using defaults.' );
 }
 
 
 var AugustBot = new Discord.Client();
-var commands  = {
+var Commands  = {
 	'ping' : {
-	  description: "",
+	  description: "Replies with 'pong'; basically a debugging/troubleshooting command.",
 	  process: function( bot, msg, suff ) {
-	  	bot.sendMessage( msg.channel, "Pong, bitch!" );
+	  	bot.reply( msg, "pong, bitch!" );
 	  }
+	},
+	
+	'shutdown' : {
+		usage: '<timeout>',
+		descsription: "Ends the server process running bot on the host server. Default timeout is 5s, otherwise provide the desired pause time in millisections.",
+		process: function( bot, msg, suff ) {
+			try {
+				var timeout = ( suff ) ? parseInt( suff ) : 5000;
+				bot.sendMessage( msg.channel, '`Shutdown` command issued by: ***' + msg.author + '***\n'
+											 + 'Shutting down in ' + timeout/1000 + ' seconds...' );
+				setTimeout( function(){ process.exit(); }, timeout );				
+			} catch( e ) {
+				handleInStride( e, msg );
+			}
+		}
+	},
+	
+	'gif' : {
+		usage: '<tags>',
+		description: 'Get a random gif, filtered by the supplied tag(s), from the GiphyAPI',
+		process: function( bot, msg, suff ) {
+			var tags = suff.split( ' ' );
+			if( tags.length < 1 ) bot.reply( msg, ' you must provide tags for me to find a gif' );
+			try {
+				var giphyApiKey = 'dc6zaTOxFJmzC', // Move to Config or something
+						giphyURL    = 'http://api.giphy.com/v1/gifs/random?api_key=' + giphyApiKey + '&tag=' + tags.join( '+' );
+				
+				Request( giphyURL, function( error, response, body ) {
+					// Giphy API retunrs json array
+					var gifObj = JSON.parse( body );
+					if( error || response.statusCode != 200 ) {
+						console.error( 'Error accessing Giphy API: ' + body );
+						console.log( error );
+					} else {
+						// { data: { *Gif Data }, meta: { *Response data } }
+						bot.sendMessage( msg.channel, gifObj.data.url );
+					}
+				});
+			} catch( e ) {
+				handleInStride( e, msg );
+			}
+		}
 	}
 };
 
@@ -49,17 +94,17 @@ AugustBot.on( 'disconnected', function() {
 AugustBot.on( 'message', function( message ) {
 	if( message.author.id != AugustBot.user.id
 	    && ( message.content[ Config.commandPrefix.length - 1 ] === Config.commandPrefix
-	         || message.content.indexOf( AugustBot.user.mention() ) == 0 ) ){
+	         || message.content.indexOf( AugustBot.user.mention() ) == 0 ) ) {
 		
-		console.log( message.auther + ' attempting command: ' + message.content );
+		console.log( message.author + ' attempting command: ' + message.content );
 	
 		var commandText = message.content.split( ' ' )[0].substring( 1 );
 		var commandSuffix = message.content.substring( commandText.length + 1 + Config.commandPrefix.length );
-		var cmd = commands[ commandText ];
-
-		if( cmd ) {
+		var action = Commands[ commandText ];
+		
+		if( action ) {
 			try {
-				AugustBot.reply( message, '`' + commandText + '` command found!' );
+				action.process( AugustBot, message, commandSuffix );
 			} catch (e) {
 				if( Config.debug ) {
 					AugustBot.reply( message, '`' + commandText + '` command failed to execute!\n' + e.stack );
@@ -74,10 +119,21 @@ AugustBot.on( 'message', function( message ) {
 	} else {
 		if( message.author == AugustBot.user ) return;
 		if( message.author != AugustBot.user && message.isMentioned( AugustBot.user ) ){
-			AugustBot.reply( message, "Shut the fuck up." );
+			AugustBot.reply( message, "shut the fuck up." );
 		}
 	}
 });
 
 
 AugustBot.loginWithToken( AuthInfo.token );
+
+	function handleInStride( e, msg ) {
+		if( Config.debug ) {
+			AugustBot.sendMessage( msg.channel, '***Command failed!***\n'
+											+ '```\n ' + e.stack + '\n```' );
+		} else {
+			console.error( 'Command failed!' );
+			console.log( e.stack );
+			AugustBot.reply( msg, 'your command failed. See console for details.' );
+		}
+	}
